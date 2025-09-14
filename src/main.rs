d93ae89e9a213f60aec66d295d402a3bbd8dd7b3
Fouad Sfarijlani <fouad.sfarijlani@hotmail.com>
2025-09-14 16:45:46 +0200

    feat: validate method on MarbleInteger

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,10 @@ fn main() {
     new_int.add_less_than_constraint(4);
     println!("{:?}", new_int);
 
+    new_int.constraint = None;
+    let results = new_int.validate(7);
+    println!("{:?}", results);
+
     match &new_int.constraint {
         Some(c) => println!("value 7 is {:?}", c.validate(7)),
         None => println!("No constraint found"),
